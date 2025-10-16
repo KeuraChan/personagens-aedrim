4fc3c332e38140a56aa523fa640fe91c1441d080
Keura <109036590+KeuraChan@users.noreply.github.com>
2025-10-16 10:47:03 -0300

    Update tags.js

--- data/tags.js
+++ data/tags.js
@@ -1,1 +1,46 @@
 
+// Arquivo: tags.js
+
+// Categorias de tags
+
+const RACA = {
+  HUMANO: "Humano",
+  ELFO: "Elfo",
+  ANAO: "Anão",
+  DRAGAO: "Dragão",
+  MEIO_DRAGAO: "Dragão",
+  DEMONIO: "Uro",
+  BESTA: "Besta",
+};
+
+const CLASSE = {
+  GUERREIRO: "Guerreiro",
+  MAGO: "Mago",
+  ARQUEIRO: "Arqueiro",
+  LADINO: "Ladino",
+  SACERDOTE: "Sacerdote",
+};
+
+const OCUPACAO = {
+  HEROI: "Herói",
+  INIMIGO: "Inimigo",
+  NPC: "NPC",
+  VENDEDOR: "Vendedor",
+  CHEFE: "Chefe",
+  TAVERNEIRO: "Taverneiro",
+  LIDER_GUILDA: "Líder de Guilda"
+};
+
+const CIDADE = {
+  DRAGAO_DORMENTE: "Dragão Dormente",
+  BADUR: "Badür",
+  ARCO_GELIDO: "Arco Gélido",
+};
+
+// Se quiser uma lista geral de todas as tags (para montar o menu de filtros)
+const TODAS_AS_TAGS = {
+  Raca: RACA,
+  Classe: CLASSE,
+  Ocupacao: OCUPACAO,
+  Cidade: CIDADE,
+};
