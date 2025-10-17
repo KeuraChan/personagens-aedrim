da335541a86af45cf6b0046c71de169185646f5f
Keura <109036590+KeuraChan@users.noreply.github.com>
2025-10-16 23:35:57 -0300

    Update personagens.js

--- data/personagens.js
+++ data/personagens.js
@@ -1,7 +1,7 @@
 var personagens = [
   {
     nome: "Amargo",
-    imagens: ["Amargo1.jpg", "Amargo2.png"],
+    imagens: ["Amargo1.png"],
     resumo: "Elfo das profundezas, líder de dragão dormente e faz juz ao nome.",
     descricao: "Líder da Guilda Revival Chronicles em Taverna do Dragão Dormente desde 4400, Amargo é severo, grosso e contantemente descarta a utilidade de aventureiros que não se provam, perdeu seu braço direito, alguns dedos da mão esquerda, parte da mobilidade de seu joelho e sua boca é rasgada do queixo ao nariz.",
     nascimento: "85º do Inverno, ano 3984",
@@ -9,7 +9,7 @@ var personagens = [
   },
   {
     nome: "Elkas",
-    imagens: ["Elkas1.jpg"],
+    imagens: ["Elkas1.png"],
     resumo: "Taverneiro de Dragão Dormente",
     descricao: "Alegre Elfo da Lua. Se tornou Taverneiro já há séculos, sempre com um sorriso no rosto, uma espada sempre embainhada na cintura, é amigável, sempre cumprimentando todos que chegam e disposto a oferecer bebidas.",
     nascimento: "???",
